56f796aeea4b4d2cbfdd8d5c163d80feb4d93955
VladRidun <ridunvv@gmail.com>
2024-05-14 10:43:43 +0400

    4. Время жизни (Scope) [#261277]

--- src/main/java/ru/job4j/di/tracker/service/StartUI.java
+++ src/main/java/ru/job4j/di/tracker/service/StartUI.java
@@ -1,17 +1,23 @@
 package ru.job4j.di.tracker.service;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import ru.job4j.di.tracker.controller.ConsoleInput;
 import ru.job4j.di.tracker.repository.Store;
 
 @Component
+@Scope("prototype")
 public class StartUI {
-    @Autowired
+
     private Store store;
-    @Autowired
+
     private ConsoleInput consoleInput;
+
+    public StartUI(Store store, ConsoleInput consoleInput) {
+        this.store = store;
+        this.consoleInput = consoleInput;
+    }
 
     public void add(String value) {
         String str = consoleInput.askStr(value);
